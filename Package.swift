e3752253c37349086a8f6dc8ac7450054048ff8a
zig <zig1375@gmail.com>
2017-08-29 16:09:32 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
   targets:      [],
   dependencies: [
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 16, minor: 0),
-      .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 12)
+      .Package(url: "https://github.com/Zig1375/BlueSocket.git", majorVersion: 0, minor: 12)
   ]
 )
