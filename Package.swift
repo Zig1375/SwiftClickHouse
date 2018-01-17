26fe5426d9ef1458126e07085cb69bb1d63369fb
zig <zig1375@gmail.com>
2018-01-17 17:24:07 +0300

    fixes for swift4

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name:         "SwiftClickHouse",
   targets:      [],
   dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 16, minor: 0),
+      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 17, minor: 0),
       .Package(url: "https://github.com/Zig1375/BlueSocket2.git", majorVersion: 0, minor: 12)
   ]
 )
